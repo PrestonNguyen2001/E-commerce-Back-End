3c3b27897344c3018982516ddc5c2fde22b1db01
PrestonNguyen2001 <prestonnguyen2001@gmail.com>
2024-05-20 21:51:57 -0400

    Created server and connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,25 @@
-require('dotenv').config();
-
-const Sequelize = require('sequelize');
+// connection.js
+require("dotenv").config();
+const Sequelize = require("sequelize");
 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "postgres",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected..."))
+  .catch((err) => console.log("Error: " + err));
 
 module.exports = sequelize;
